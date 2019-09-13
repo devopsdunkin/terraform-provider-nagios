46f74007b809f139d6d89d50690425b7fd5c8751
Chris Dunkin <cdunkin@hy-vee.com>
2019-09-12 20:14:46 -0500

    Adds functions to support connecting to Nagios

--- nagios/client.go
+++ nagios/client.go
@@ -1,11 +1,13 @@
 package nagios
 
 import (
-	"bytes"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -32,9 +34,9 @@ func NewClient(url, token string) *Client {
 }
 
 func (c *Client) sendRequest(httpRequest *http.Request) ([]byte, error) {
+	c.addRequestHeaders(httpRequest)
+
 	response, err := c.httpClient.Do(httpRequest)
-	log.Printf("[DEBUG] Request body: [%p]", httpRequest)
-	log.Printf("[DEBUG] HTTP Status code: %d", response.StatusCode)
 
 	if err != nil {
 		log.Printf("[ERROR] Error occurred completing HTTP request: %s", err.Error())
@@ -44,7 +46,6 @@ func (c *Client) sendRequest(httpRequest *http.Request) ([]byte, error) {
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
-	log.Printf("[DEBUG] HTTP response body: [%p]", body)
 
 	if err != nil {
 		log.Printf("[ERROR] Error occurred reading body: %s", err.Error())
@@ -54,10 +55,73 @@ func (c *Client) sendRequest(httpRequest *http.Request) ([]byte, error) {
 	return body, nil
 }
 
-func (c *Client) get(objectURL string, resourceInfo interface{}) error {
-	nagiosURL := c.url + objectURL + "?apikey=" + c.token + "&pretty=1"
+func (c *Client) buildURL(objectType, method, objectName, name string) (string, error) {
+	var nagiosURL strings.Builder
 
-	request, err := http.NewRequest(http.MethodGet, nagiosURL, nil)
+	var apiURL string
+	if strings.HasSuffix(c.url, "/") {
+		apiURL = "api/v1/config/"
+	} else {
+		apiURL = "/api/v1/config/"
+	}
+
+	// All of this creates the nagiosURL to get the object
+	// that has been supplied through parameters
+	nagiosURL.WriteString(c.url)
+	nagiosURL.WriteString(apiURL)
+	nagiosURL.WriteString(objectType)
+
+	// If we are doing a GET, PUT or DELETE, we need to provide the name of the object
+	// and type to filter results to only that. Otherwise, Nagios
+	// will return all results for that particular object type
+	// TODO: This is getting messy. Need to figure out a more streamlined way to handle all of this
+	if method == "GET" || method == "DELETE" {
+		nagiosURL.WriteString("?apikey=")
+		nagiosURL.WriteString(c.token)
+		nagiosURL.WriteString("&")
+		nagiosURL.WriteString(objectName)
+		nagiosURL.WriteString("=")
+
+		if name == "" {
+			errMsg := "Name must be provided when using the " + method + " method"
+			log.Printf("[ERROR] %s", errMsg)
+			return "", errors.New(errMsg)
+		} else {
+			nagiosURL.WriteString(name)
+		}
+	} else if method == "PUT" {
+		nagiosURL.WriteString("/")
+		nagiosURL.WriteString(name)
+		nagiosURL.WriteString("?apikey=")
+		nagiosURL.WriteString(c.token)
+		nagiosURL.WriteString("&pretty=1&applyconfig=1")
+		// nagiosURL.WriteString()
+	} else if method == "POST" {
+		nagiosURL.WriteString("?apikey=")
+		nagiosURL.WriteString(c.token)
+		nagiosURL.WriteString("&applyconfig=1")
+	}
+
+	if method != "GET" {
+		nagiosURL.WriteString("&applyconfig=1") // During a POST/PUT/DELETE, we want to tell Nagios to update the config
+	} else {
+		nagiosURL.WriteString("&pretty=1") // Otherwise, if just doing a GET, we want it returned in a pretty format (JSON)
+	}
+
+	log.Printf("[DEBUG] Nagios URL - %s", nagiosURL.String()) // TODO: Need to scrub API key from logs
+
+	return nagiosURL.String(), nil
+}
+
+func (c *Client) addRequestHeaders(request *http.Request) {
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Add("Accept", "/")
+
+	return
+}
+
+func (c *Client) get(data *url.Values, resourceInfo interface{}, nagiosURL string) error {
+	request, err := http.NewRequest(http.MethodGet, nagiosURL, strings.NewReader(data.Encode()))
 
 	if err != nil {
 		log.Printf("[ERROR] Error occurred during request: %s", err.Error())
@@ -65,39 +129,73 @@ func (c *Client) get(objectURL string, resourceInfo interface{}) error {
 	}
 
 	body, err := c.sendRequest(request)
+	// log.Printf("[DEBUG] Body - %s", string(body))
 
 	if err != nil {
 		log.Printf("[ERROR] Error occurred sending request: %s", err.Error())
 		return err
 	}
 
+	if err != nil {
+		log.Printf("[ERROR] Error unmarshaling JSON data from Nagios - %s", err.Error())
+		return err
+	}
+
+	test := body
+	log.Printf("[DEBUG] Test value - %s", string(test))
+
 	return json.Unmarshal(body, resourceInfo)
 }
 
-func (c *Client) post(configURL string, requestBody interface{}) ([]byte, error) {
-	nagiosURL := c.url + configURL + "?apikey=" + c.token + "&applyconfig=1"
-	data, err := json.Marshal(requestBody)
-	// log.Printf("[DEBUG] Request body: [%p]", requestBody)
-	// log.Printf("[DEBUG] data variable value: [%p]", data)
+func (c *Client) post(data *url.Values, nagiosURL string) ([]byte, error) {
+	request, err := http.NewRequest(http.MethodPost, nagiosURL, strings.NewReader(data.Encode()))
 
 	if err != nil {
-		log.Printf("[ERROR] Error occurred: %s", err.Error())
-		return nil, err
-	}
-
-	request, err := http.NewRequest(http.MethodPost, nagiosURL, bytes.NewReader(data))
-	log.Printf("[DEBUG] HTTP request body: [%p]", request.Body)
-
-	if err != nil {
-		log.Printf("[ERROR] Error occurred creating request: %s", err.Error())
+		log.Printf("[ERROR] Error creating HTTP request - %s", err.Error())
 		return nil, err
 	}
 
 	body, err := c.sendRequest(request)
-	log.Printf("[DEBUG] Response from Nagios - %s", string(body))
+	// log.Printf("[DEBUG] Response from Nagios - %s", string(body))
 
 	if err != nil {
-		log.Printf("[ERROR] Error occurred sending request: %s", err.Error())
+		log.Printf("[ERROR] Error sending request: %s", err.Error())
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func (c *Client) put(data *url.Values, nagiosURL string) ([]byte, error) {
+	request, err := http.NewRequest(http.MethodPut, nagiosURL, strings.NewReader(data.Encode()))
+
+	if err != nil {
+		log.Printf("[ERROR] Error creating HTTP request - %s", err.Error())
+		return nil, err
+	}
+
+	body, err := c.sendRequest(request)
+
+	if err != nil {
+		log.Printf("[ERROR] Error sending request - %s", err.Error())
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func (c *Client) delete(data *url.Values, nagiosURL string) ([]byte, error) {
+	request, err := http.NewRequest(http.MethodDelete, nagiosURL, strings.NewReader(data.Encode()))
+
+	if err != nil {
+		log.Printf("[ERROR] Error creating HTTP request - %s", err.Error())
+		return nil, err
+	}
+
+	body, err := c.sendRequest(request)
+
+	if err != nil {
+		log.Printf("[ERROR] Error sending request - %s", err.Error())
 		return nil, err
 	}
 
