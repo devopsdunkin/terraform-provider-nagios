bfb59d9780c964ff7fb2bbf2f8e2108f69e29dd0
Chris Dunkin <cdunkin@hy-vee.com>
2019-09-29 13:26:40 -0500

    Adds provider test cases

---
+++ nagios/provider_test.go
@@ -0,0 +1,37 @@
+package nagios
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+var requiredEnvVariables = []string{
+	"API_TOKEN",
+	"NAGIOS_URL",
+}
+
+func TestNagiosProvider(t *testing.T) {
+	if err := testAccProvider.InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func testAccPreCheck(t *testing.T) {
+	for _, variable := range requiredEnvVariables {
+		if value := os.Getenv(variable); value == "" {
+			t.Fatalf("%s must be set before running acceptance tests.", variable)
+		}
+	}
+}
+
+func init() {
+	testAccProvider = NagiosProvider()
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"nagios": testAccProvider,
+	}
+}
