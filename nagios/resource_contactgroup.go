34e64616ca336e569300b4f5276f0442e15e0d2e
Chris Dunkin <cdunkin@hy-vee.com>
2019-11-20 19:52:14 -0600

    Adds resource contactgroup

---
+++ nagios/resource_contactgroup.go
@@ -0,0 +1,159 @@
+package nagios
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+// Host contains all info needed to create a host in Nagios
+// TODO: Test to see if we need both JSON and schema tags
+// Using tag with both JSON and schema because a POST uses URL encoding to send data
+
+// TODO: Need to add in all of the other fields. What we have right now will work for initial testing
+type Contactgroup struct {
+	ContactgroupName    string        `json:"contactgroup_name"`
+	Alias               string        `json:"alias"`
+	Members             []interface{} `json:"members"`
+	ContactgroupMembers []interface{} `json:"contactgroup_members"`
+}
+
+/*
+	For any bool value, we allow the user to provide a true/false value, but you will notice
+	that we immediately convert it to its integer form and then to a string. We want to provide
+	the user with an easy to use schema, but Nagios wants the data as a one or zero in string format.
+	This seemed to be the easiest way to accomplish that and I wanted to note why it was done that way.
+*/
+
+func resourceContactgroup() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"contactgroup_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The name of the contact group",
+			},
+			"alias": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Determines whether or not the contact will receive notifications about host problems and recoveries",
+			},
+			"members": {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "A list of the short names of the commands used to notify the contact of a host problem or recovery. Multiple notification commands should be separated by commas. All notification commands are executed when the contact needs to be notified",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"contactgroup_members": {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "A list of the short names of the commands used to notify the contact of a service problem or recovery. Multiple notification commands should be separated by commas. All notification commands are executed when the contact needs to be notified",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+		Create: resourceCreateContactgroup,
+		Read:   resourceReadContactgroup,
+		Update: resourceUpdateContactgroup,
+		Delete: resourceDeleteContactgroup,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+	}
+}
+
+func resourceCreateContactgroup(d *schema.ResourceData, m interface{}) error {
+	nagiosClient := m.(*Client)
+
+	contactgroup := setContactgroupFromSchema(d)
+
+	_, err := nagiosClient.newContactgroup(contactgroup)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(contactgroup.ContactgroupName)
+
+	return resourceReadContactgroup(d, m)
+}
+
+func resourceReadContactgroup(d *schema.ResourceData, m interface{}) error {
+	nagiosClient := m.(*Client)
+
+	contactgroup, err := nagiosClient.getContactgroup(d.Id())
+
+	if err != nil {
+		return err
+	}
+
+	if contactgroup == nil {
+		// contact not found. Let Terraform know to delete the state
+		d.SetId("")
+		return nil
+	}
+
+	setDataFromContactgroup(d, contactgroup)
+
+	return nil
+}
+
+func resourceUpdateContactgroup(d *schema.ResourceData, m interface{}) error {
+	nagiosClient := m.(*Client)
+
+	contactgroup := setContactgroupFromSchema(d)
+
+	oldVal, _ := d.GetChange("contactgroup_name")
+
+	if oldVal == "" {
+		oldVal = d.Get("contactgroup_name").(string)
+	}
+
+	err := nagiosClient.updateContactgroup(contactgroup, oldVal)
+
+	if err != nil {
+		return err
+	}
+
+	setDataFromContactgroup(d, contactgroup)
+
+	return resourceReadContactgroup(d, m)
+}
+
+func resourceDeleteContactgroup(d *schema.ResourceData, m interface{}) error {
+	nagiosClient := m.(*Client)
+
+	_, err := nagiosClient.deleteContactgroup(d.Id())
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setDataFromContactgroup(d *schema.ResourceData, contactgroup *Contactgroup) {
+	d.SetId(contactgroup.ContactgroupName)
+	d.Set("contactgroup_name", contactgroup.ContactgroupName)
+	d.Set("alias", contactgroup.Alias)
+
+	if contactgroup.Members != nil {
+		d.Set("members", contactgroup.Members)
+	}
+
+	if contactgroup.ContactgroupMembers != nil {
+		d.Set("contactgroup_members", contactgroup.ContactgroupMembers)
+	}
+}
+
+func setContactgroupFromSchema(d *schema.ResourceData) *Contactgroup {
+	contactgroup := &Contactgroup{
+		ContactgroupName:    d.Get("contactgroup_name").(string),
+		Alias:               d.Get("alias").(string),
+		Members:             d.Get("members").(*schema.Set).List(),
+		ContactgroupMembers: d.Get("contactgroup_members").(*schema.Set).List(),
+	}
+
+	return contactgroup
+}
---
+++ nagios/resource_contactgroup_test.go
@@ -0,0 +1,177 @@
+package nagios
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccContactgroup_basic(t *testing.T) {
+	contactgroupName := "tf_" + acctest.RandString(10)
+	contactgroupAlias := "tf_" + acctest.RandString(10)
+	contactgroupMembers := "nagiosadmin"
+	resourceName := "nagios_contactgroup.contactgroup"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckContactgroupDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContactgroupResource_basic(contactgroupName, contactgroupAlias, contactgroupMembers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContactgroupExists(resourceName),
+				),
+			},
+		},
+	})
+}
+
+func TestAccContactgroup_createAfterManualDestroy(t *testing.T) {
+	var contactgroup = &Contactgroup{}
+	contactgroupName := "tf_" + acctest.RandString(10)
+	contactgroupAlias := "tf_" + acctest.RandString(10)
+	contactgroupMembers := "nagiosadmin"
+	resourceName := "nagios_contactgroup.contactgroup"
+
+	resource.Test(t, resource.TestCase{
+		Providers:    testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		CheckDestroy: testAccCheckContactgroupDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContactgroupResource_basic(contactgroupName, contactgroupAlias, contactgroupMembers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContactgroupExists(resourceName),
+					testAccCheckContactgroupFetch(resourceName, contactgroup),
+				),
+			},
+			{
+				PreConfig: func() {
+					client := testAccProvider.Meta().(*Client)
+
+					_, err := client.deleteContactgroup(contactgroup.ContactgroupName)
+					if err != nil {
+						t.Fatal(err)
+					}
+				},
+				Config: testAccContactgroupResource_basic(contactgroupName, contactgroupAlias, contactgroupMembers),
+				Check:  testAccCheckContactgroupExists(resourceName),
+			},
+		},
+	})
+}
+
+func TestAccContactgroup_updateName(t *testing.T) {
+	firstContactgroupName := "tf_" + acctest.RandString(10)
+	secondContactgroupName := "tf_" + acctest.RandString(10)
+	contactgroupAlias := "tf_" + acctest.RandString(10)
+	contactgroupMembers := "nagiosadmin"
+	resourceName := "nagios_contactgroup.contactgroup"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckContactgroupDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccContactgroupResource_basic(firstContactgroupName, contactgroupAlias, contactgroupMembers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContactgroupExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "contactgroup_name", firstContactgroupName),
+				),
+			},
+			{
+				Config: testAccContactgroupResource_basic(secondContactgroupName, contactgroupAlias, contactgroupMembers),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckContactgroupExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "contactgroup_name", secondContactgroupName),
+				),
+			},
+		},
+	})
+}
+
+func testAccContactgroupResource_basic(contactgroupName, alias, members string) string {
+	return fmt.Sprintf(`
+resource "nagios_contactgroup" "contactgroup" {
+	contactgroup_name	= "%s"
+	alias				= "%s"
+	members				= [
+		"%s"
+	]
+}
+	`, contactgroupName, alias, members)
+}
+
+func testAccCheckContactgroupDestroy() resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		for _, rs := range s.RootModule().Resources {
+			if rs.Type != "nagios_contactgroup" {
+				continue
+			}
+
+			// Get the name from the state and check if it still exists
+			name := rs.Primary.Attributes["contactgroup_name"]
+
+			conn := testAccProvider.Meta().(*Client)
+
+			contactgroup, _ := conn.getContactgroup(name)
+			if contactgroup.ContactgroupName != "" {
+				return fmt.Errorf("Contact group %s still exists", name)
+			}
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckContactgroupExists(resourceName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		_, err := getContactgroupFromState(s, resourceName)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func getContactgroupFromState(s *terraform.State, resourceName string) (*Contactgroup, error) {
+	nagiosClient := testAccProvider.Meta().(*Client)
+	rs, ok := s.RootModule().Resources[resourceName]
+	if !ok {
+		return nil, fmt.Errorf("contact group not found: %s", resourceName)
+	}
+
+	name := rs.Primary.Attributes["contactgroup_name"]
+	log.Printf("[DEBUG] Name value from state - %s", name)
+
+	contactgroup, err := nagiosClient.getContactgroup(name)
+
+	if err != nil {
+		return nil, fmt.Errorf("error getting contact group with name %s: %s", name, err)
+	}
+
+	return contactgroup, nil
+}
+
+func testAccCheckContactgroupFetch(resourceName string, contactgroup *Contactgroup) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		returnedContactgroup, err := getContactgroupFromState(s, resourceName)
+		if err != nil {
+			return err
+		}
+
+		contactgroup.ContactgroupName = returnedContactgroup.ContactgroupName
+		contactgroup.Alias = returnedContactgroup.Alias
+		contactgroup.Members = returnedContactgroup.Members
+		contactgroup.ContactgroupMembers = returnedContactgroup.ContactgroupMembers
+
+		return nil
+	}
+}
